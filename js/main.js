24986e7afaf68a459f97434c707b6b3a8aaba70c
garciamarvs <garcia.marvs3@gmail.com>
2022-04-15 12:53:26 +0800

    update infos

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-const doodle = document.querySelector('css-doodle');
-
-doodle.addEventListener('click', updateCover);
-doodle.addEventListener('mouseover', updateCover);
-
-function updateCover () {
-  const doodle = document.querySelector('css-doodle');
-
-  doodle.update();
-}
-
-function goTo (id) {
+function goTo(id) {
   var tab = document.getElementById('v-tabs-' + id);
 
   tab.scrollIntoView({ behavior: 'smooth' });
